ab97a2453bfed7dfa90b84b70dafb9aa68e4edcd
shivkumar <hallaleshivu16@gmail.com>
2022-01-02 16:13:17 +0530

    ADDED: Line Comparison By Ends

--- src/LineComparison/UC3LineComparisonByEndPoints.java
+++ src/LineComparison/UC3LineComparisonByEndPoints.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class UC3LineComparisonByEndPoints {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double[] length = new double[2];
+        double length [] = new double[2];
 //        taking inputs
         for (int i = 0; i < 2; i++) {
-            System.out.println((i + 1) + ": Enter the values for x1, y1, x2, y2");
+            System.out.println("Enter the values for Line:" + (i + 1) + " x1, y1, x2, y2");
             int x1 = sc.nextInt();
             int y1 = sc.nextInt();
             int x2 = sc.nextInt();
@@ -23,10 +23,10 @@ public class UC3LineComparisonByEndPoints {
             System.out.println("The length of two lines are equal.");
         }
         else if(length[0] > (length[1])){
-            System.out.println("Length of line 0 is greater than the length of line 1");
+            System.out.println("Length of line 1 is greater than the length of line 2");
         }
         else{
-            System.out.println("Length of line 1 is greater than the length of line 0");
+            System.out.println("Length of line 2 is greater than the length of line 1");
         }
     }
 }
