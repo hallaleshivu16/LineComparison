a6f5176514a365160c9117fde5cb15e5e549ab74
shivkumar <hallaleshivu16@gmail.com>
2022-01-02 15:34:29 +0530

    ADDED: Equality of Two Lines

---
+++ src/LineComparison/UC2.java
@@ -0,0 +1,29 @@
+package LineComparison;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class UC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double length[] = new double[2];
+//        taking inputs
+        for (int i = 0; i < 2; i++) {
+            System.out.println((i + 1) + ": Enter the values for x1, y1, x2, y2");
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+//            calculating length of the line and storing in length array
+            length[i] = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            System.out.println("Length of line " + (i + 1) + ": " + length[i]);
+        }
+//        checking equality of two lengths
+        if(Objects.equals(length[0], length[1])){
+            System.out.println("The length of two lines are equal.");
+        }
+        else{
+            System.out.println("The length of two lines are not equal.");
+        }
+    }
+}
